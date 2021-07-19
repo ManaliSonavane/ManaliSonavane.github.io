aad365c1276cceb76d209c3d1db796385c5ea6ac
Manali Sonavane <manalisonavane1@gmail.com>
2021-07-19 08:56:22 -0700

    Update script.js

--- mod5/script.js
+++ mod5/script.js
@@ -14,9 +14,9 @@ names[9]="jim";
 for(var i=0;i<names.length;i++)
 {
   if(names[i].charAt(0)=='J' || names[i].charAt(0)=='j' ){
-  console.log("Goodbye"+ names[i]);
+  console.log("Goodbye "+ names[i]);
   }
   else{
-    console.log("hello"+ names[i]);
+    console.log("hello "+ names[i]);
   }
 }
